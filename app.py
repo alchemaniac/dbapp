1e9ca59d0a29f773a03a74d666cfff857b8fa642
Rishabh Jain <45866679+alchemaniac@users.noreply.github.com>
2019-09-02 23:29:15 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+  host="10.246.73.98",
+  user="super",
+  password="mysql12345"
+)
+
+mycursor = mydb.cursor()
+try:
+	mycursor.execute("CREATE DATABASE mydatabase")
+except:
+	print('db exists')
+mycursor.execute("USE mydatabase")
+try:
+	mycursor.execute("CREATE TABLE somedata (name VARCHAR(255), id VARCHAR(255))")
+except:
+	print('table exists')
+query = """INSERT INTO somedata VALUES (%s, %s) """
+@app.route('/',methods=['GET','POST'])
+def index():
+	if request.method=="POST":
+		details=request.form
+		tname=details['name']
+		tid=details['id']
+		indata=(tname,tid)
+		mycursor.execute(query, indata)
+		mycursor.execute("commit;")
+	mycursor.execute("select * from somedata")
+	result=mycursor.fetchall()
+	pname=[]
+	pid=[]
+	if(result!=[]):
+		for data in result:
+			pname.append(data[0])
+			pid.append(data[1])
+	return render_template('index.html',pname=pname,pid=pid)
+
+if __name__ == "__main__":
+	app.run(host="localhost")
